2a4393090acb3839796369df8703e53ffb556f0f
Victor Alagwu <victoralagwu@gmail.com>
2017-11-18 08:12:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+  var sum = a + b;
+  return sum;
+}
+function subtract(a, b){
+  var minus = a - b;
+  return minus;
+}
+function multiply(a ,b) {
+  var times = a * b;
+  return times;
+}
+function divide(a, b){
+  var div = a/b;
+  return div;
+}
+function inc(n){
+  n += 1;
+  return n;
+}
+function dec(n) {
+  n -= 1
+ return n;
+}
+function makeInt(n) {
+  var integerNum = parseInt(n,10);
+  return integerNum;
+}
+function preserveDecimal(n) {
+   var float_num = parseFloat(n);
+   return float_num;
+}
