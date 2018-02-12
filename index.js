517c83960a763bd45f269566d0a896375f49c372
Raphael Schinköth <rapha@schinkoeth.com>
2018-02-12 20:33:14 +0100

    done

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+
+}
+
+function multiply(a, b) {
+    return a * b
+
+}
+
+function divide(a, b) {
+    return (a/b)
+}
+function inc(a) {
+    var num = a
+    num++
+    return num
+}
+function dec(a) {
+    var num = a
+    num--
+    return num
+
+}
+function makeInt(a) {
+    return parseInt(a, 10)
+
+}
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
