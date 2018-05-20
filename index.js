02e3ec8150fe697ffafc257459f3e737eb4ba1ee
Ronel Aguilar <raguilar@umd.edu>
2018-05-20 21:28:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const add = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+const inc = n => ++n;
+
+const dec = n => --n;
+
+const makeInt = n => parseInt(n, 10);
+
+const preserveDecimal = n => parseFloat(n);
