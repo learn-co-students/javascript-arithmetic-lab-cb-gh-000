0a3bef8554f7a49444afc837166343c720129df2
Dayanidhee Singh <dayanidhi.singh.sd@mmumullana.org>
2018-11-24 10:10:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b){
+  var sum=0;
+  sum = a+b;
+  return sum;
+}
+function subtract(a, b){
+  var x=0;
+  x = a-b;
+  return x;
+}
+function multiply(a, b){
+  var y=0;
+  y = a*b;
+  return y;
+}
+function divide(a, b){
+  var c=0;
+  c = a/b;
+  return c;
+}
+function inc(a){
+  a++
+  return a;
+}
+function dec(a){
+  a=a-1;
+  return a;
+}
