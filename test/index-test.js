a1108dafda9e86ec09de85967df0354baaea54fc
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-02-18 15:32:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -52,3 +52,37 @@ describe('preserveDecimal(n)', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
   })
 })
+
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(n){
+  n++;
+  return n;
+}
+
+function dec(n){
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+
+}
