a17426a1d7714e6b1a271716a86cda2c2ff514c2
Andrea Spadavecchia <spadavecchia.andrea@spes.uniud.it>
2019-05-07 15:11:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const inc = (a) => ++a;
+
+const dec = (a) => --a;
+
+const makeInt = (str) => parseInt(str, 10);
+
+const preserveDecimal = (str) => parseFloat(str, 10);
