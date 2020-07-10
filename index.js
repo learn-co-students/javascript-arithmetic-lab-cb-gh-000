782d746f76db7abd4b289bfadd33cfa6bb867875
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-07-10 18:09:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+  return a+b;
+}
+
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function increment(n) {
+  return n += 1;
+}
+
+function decrement(n) {
+  return n -= 1;
+}
+
+function makeInt(n) {
+  var nInt = parseInt(n, 10);
+  return nInt;
+}
+
+function preserveDecimal(n) {
+  var nfloat = parseFloat(n);
+  return nfloat;
+}
